a4e75d6ae8dc876461a1f916bb12a138f8ce35ac
Natnael Demisse <natnael.demisse@siriusxm.com>
2020-05-12 19:47:13 -0500

    add error codes

--- enum/constants.ts
+++ enum/constants.ts
@@ -1,7 +1,10 @@
 export enum ErrorCode {
     LOGIN_ERROR = "LOGIN_ERROR",
-    INVALID_PARAM = ""
-
+    INVALID_PARAM = "INVALID_PARAM",
+    REGISTER_ERROR = "SIGNUP_ERROR",
+    PROFILE_ERROR = "PROFILE_ERROR",
+    INVALID_AUTH = "INVALID_AUTH",
+    APP_TOKEN_ERROR = "APP_TOKEN_ERROR"
 }
 
 export enum ErrorMessage {
