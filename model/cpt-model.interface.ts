22b8d62b437d4ceaef11e0a2a8a7cdcd98ef6c59
Natnael Demisse <natnael.demisse@siriusxm.com>
2020-05-19 16:21:26 -0500

    update media model

--- model/cpt-model.interface.ts
+++ model/cpt-model.interface.ts
@@ -16,33 +16,3 @@ interface Rendered {
     rendered: string;
     protected?:boolean;
 }
-
-export interface MediaModel {
-    media_details: MediaDetails;
-    alt_text: string,
-    media_type: string,
-    mime_type: string;
-    description: Rendered;
-    caption: Rendered;
-}
-
-interface MediaDetails {
-    width: number;
-    height: number;
-    file:string;
-    sizes:Sizes;
-}
-
-interface Sizes {
-    thumbnail:FileContent;
-    full:FileContent;
-}
-
-interface FileContent {
-    file:string;
-    width:number;
-    height:number;
-    mime_type:string;
-    source_url:string;
-}
-
