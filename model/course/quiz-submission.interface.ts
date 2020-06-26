e682fefa2f4dad80a0c59cffd5f85db728d53c26
Natnael Demisse <natnael.demisse@siriusxm.com>
2020-06-26 15:14:41 -0500

    Update quiz-submission.interface.ts

--- model/course/quiz-submission.interface.ts
+++ model/course/quiz-submission.interface.ts
@@ -2,7 +2,7 @@ import { QuizAnsEntry } from "./quiz-ans-entry.interface";
 
 export interface QuizSubmission extends QuizAnsEntry {
   // quiz_init_id: number;
-  total_score: number;
+  total_score?: number;
   instructor_feedback: string;
   instructor_id: number;
 }
