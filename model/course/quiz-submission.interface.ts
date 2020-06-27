d049762769ccd750b1452df7cd3d7be92143dbc7
melkam beyene <melkam3392beyu@gmail.com>
2020-06-27 23:42:09 +0300

    update

--- model/course/quiz-submission.interface.ts
+++ model/course/quiz-submission.interface.ts
@@ -1,8 +1,8 @@
 import { QuizAnsEntry } from "./quiz-ans-entry.interface";
 
 export interface QuizSubmission extends QuizAnsEntry {
-  // quiz_init_id: number;
-  total_score: number;
+  quiz_init_id: number;
+  total_score?: number;
   instructor_feedback: string;
   instructor_id: number;
 }
--- model/course/quiz.interface.ts
+++ model/course/quiz.interface.ts
@@ -1,13 +1,13 @@
-import {BaseCPTModel} from "../cpt-model.interface"
-import {AcfPostModel} from "../acf-post-model.interface"
+import { BaseCPTModel } from "../cpt-model.interface";
+import { AcfPostModel } from "../acf-post-model.interface";
 
 export interface Quiz extends BaseCPTModel {
-    acf:QuizFields;
+  acf: QuizFields;
 }
 
 interface QuizFields {
-    quiz_questions: AcfPostModel[];
-    time_limit:number;
-    quiz_num_attempts:number;
-    quiz_reward_point:number;
+  quiz_questions: AcfPostModel[];
+  time_limit: number;
+  quiz_num_attempts: number;
+  quiz_reward_point: number;
 }
